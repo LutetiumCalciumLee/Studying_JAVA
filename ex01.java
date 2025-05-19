88b5dc5541de6bb647d5ee519da1edaf1c817cb2
LutetiumCalcium <2501340038lee@gmail.com>
2025-05-20 08:50:37 +0900

    Add files via upload

---
+++ ex01.java
@@ -0,0 +1,6 @@
+// The first example to run
+public class ex01 {
+	public static void main(String[] args) {
+		System.out.println("The first example to run");
+	}
+}
---
+++ prac01.java
@@ -0,0 +1,7 @@
+// Practice 1
+public class prac01 {
+	public static void main(String[] args) {
+		System.out.print("Welcome " + "to JAVA world" );
+		System.out.print("\nSorry " + "for being so funny" );
+	}
+}
